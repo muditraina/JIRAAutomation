0b5bf83d29caca5aa2c7025b19b01dc9c599abcf
Mudit Raina <mudit_raina@persistent.com>
2021-07-11 13:30:41 +0530

    Removed unused files

--- src/in/jiraautomation/util/JIRAMessages.java
+++
@@ -1,9 +0,0 @@
-package in.jiraautomation.util;
-
-public class JIRAMessages {
-
-	
-	public static final String SITE_UNAVAILABLE = "Site temporarily unavailable";
-	public static final String ISSUE_NOT_FOUND = "Issue does not exist or you do not have permission to see it";
-	
-}
